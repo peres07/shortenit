1ac9c1c73e5dd80000117024d7b4a134ac2b1f27
José Luís <peressousa77@gmail.com>
2023-07-30 01:19:22 -0300

    chore: remove build files

--- backend/dist/routes/shorten.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import shortenController from '../controllers/shortenController.js';
-const router = express.Router();
-router.post('/shorten-url', shortenController);
-export default router;
--- backend/dist/routes/shortened.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import shortenedController from '../controllers/shortenedController.js';
-const router = express.Router();
-router.get('/:url', shortenedController);
-export default router;
