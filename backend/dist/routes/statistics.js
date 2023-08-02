b4a9213f4f1b688f279847cfe39b9049c1e4e1a0
José Luís <peressousa77@gmail.com>
2023-08-01 22:47:19 -0300

    chore: remove build files

--- backend/dist/routes/statistics.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import totalUrlsController from '../controllers/statisticsContollers/totalUrlsController.js';
-import urlController from '../controllers/statisticsContollers/urlController.js';
-const router = express.Router();
-router.get('/get-total', totalUrlsController);
-router.post('/get-url', urlController);
-export default router;
--- backend/dist/schemas/urlSchema.js
+++
@@ -1,4 +0,0 @@
-import JOI from 'joi';
-export const urlSchema = JOI.object({
-    url: JOI.string().uri().required()
-});
