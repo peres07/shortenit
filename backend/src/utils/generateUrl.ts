f168af18404b1401b459959fc1dbb89e99bfc998
José Luís <peressousa77@gmail.com>
2023-08-01 23:12:26 -0300

    feat: prevent duplicates shortened URLs

--- backend/src/utils/generateUrl.ts
+++ backend/src/utils/generateUrl.ts
@@ -1,31 +1,42 @@
 import { saveUrl } from '../database/manager/saveUrl.js'
 import { findUrl } from '../database/manager/findUrl.js'
+import { findShortened } from '../database/manager/findShortened.js'
+
+function generate (): string {
+  const length = 6
+  const characters =
+          'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let result = ''
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    )
+  }
+  return result
+}
 
 export async function generateUrl (url: string): Promise<boolean | string> {
   try {
-    const length = 6
-    const characters =
-            'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    let result = ''
-    for (let i = 0; i < length; i++) {
-      result += characters.charAt(
-        Math.floor(Math.random() * characters.length)
-      )
-    }
-
     const findUrlResult = await findUrl(url)
 
     if (findUrlResult !== false) {
       return findUrlResult.shortened_url
     }
 
-    const saveUrlResult = await saveUrl(result, url, 0, new Date())
+    let generatedUrl = generate()
+    const findShortenedResult = await findShortened(generatedUrl)
+
+    if (findShortenedResult !== false) {
+      generatedUrl = generate()
+    }
+
+    const saveUrlResult = await saveUrl(generatedUrl, url, 0, new Date())
 
     if (!saveUrlResult) {
       throw new Error('Could not save the URL')
     }
 
-    return result
+    return generatedUrl
   } catch (error) {
     console.error(error)
     return false
